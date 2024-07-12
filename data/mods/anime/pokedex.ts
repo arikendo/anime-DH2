7e4e61583fe795cf42dff1e90af787aa4200b33b
arikendo <arikendoLIVE@gmail.com>
2024-07-11 17:11:36 -0700

    dio sprite fix (please work)

--- data/mods/anime/pokedex.ts
+++ data/mods/anime/pokedex.ts
@@ -1,14 +1,12 @@
 export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 	dio: {
-		num: -40,
+		num: 9999,
 		name: "Dio",
 		types: ["Dark"],
 		genderRatio: {M: 1.0, F: 0.0},
 		baseStats: {hp: 999, atk: 999, def: 999, spa: 999, spd: 999, spe: 999},
-		abilities: {0: "Intimidate"},
+		abilities: {0: "Intimidate", H: "Moxie"},
 		heightm: 1.7,
 		weightkg: 52.2,
-		color: "Yellow",
-		eggGroups: ["Monster", "Dragon"],
 	},
 };
